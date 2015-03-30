3bb652d830501a82969a56e197b703299b923e51
Guillaume Martres <smarter@ubuntu.com>
2015-03-30 21:05:16 +0200

    Disable failing test which refines Unit
    This fails because Unit {} is temporarily transformed into
      trait <refinement> extends Unit { ... }
    by refineTypeToClass for validity checking. This is wrong because
    Unit is final (because it it a value class). The easiest way to fix
    this would be to disallow refining final classes.

--- tests/pos/t284-pos.scala
+++ tests/pos/t284-pos.scala
@@ -1,5 +1,11 @@
 trait B[T] {
-    def f1(a: T): Unit { }
+    // This fails because Unit {} is temporarily transformed into
+    //   trait <refinement> extends Unit { ... }
+    // by refineTypeToClass for validity checking. This is wrong because
+    // Unit is final (because it it a value class). The easiest way to fix
+    // this would be to disallow refining final classes.
+    //def f1(a: T): Unit { }
+
     def f2(a: T): Unit
     def f3(a: T): Unit = { }
 }
