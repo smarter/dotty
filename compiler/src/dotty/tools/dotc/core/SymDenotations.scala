229305671092dac057b439c2c7e7aa4f60d3b1de
Guillaume Martres <smarter@ubuntu.com>
2019-07-02 20:48:59 +0200

    add symbolloader

--- compiler/src/dotty/tools/dotc/core/SymDenotations.scala
+++ compiler/src/dotty/tools/dotc/core/SymDenotations.scala
@@ -297,7 +297,7 @@ object SymDenotations {
      */
     final def privateWithin(implicit ctx: Context): Symbol = {
       // ensureCompleted()
-      if(myInfo.isInstanceOf[unpickleScala2.Scala2Unpickler#LocalUnpickler | classfile.ClassfileParser#MemberCompleter | /*typer.Namer#Completer |*/ LazyType#ProxyCompleter | ModuleCompleter] ||
+      if(myInfo.isInstanceOf[unpickleScala2.Scala2Unpickler#LocalUnpickler | classfile.ClassfileParser#MemberCompleter | /*typer.Namer#Completer |*/ LazyType#ProxyCompleter | ModuleCompleter | SymbolLoader] ||
       (myInfo.isInstanceOf[typer.Namer#Completer] && !myInfo.isInstanceOf[typer.Namer#ClassCompleter])) {
         // ensureCompleted()
         completeOnce()
@@ -313,8 +313,8 @@ object SymDenotations {
 
     /** Set privateWithin. */
     protected[dotc] final def privateWithin_=(sym: Symbol): Unit = {
-      assert(!isCompleting || myInfo.isInstanceOf[unpickleScala2.Scala2Unpickler#LocalUnpickler | NoCompleter /*from ClassfileParser*/ | classfile.ClassfileParser#MemberCompleter | typer.Namer#Completer | LazyType#ProxyCompleter | ModuleCompleter], s"$this -- $myInfo -- ${myInfo.getClass}")
-      assert(isCompleted || myInfo.isInstanceOf[unpickleScala2.Scala2Unpickler#LocalUnpickler | NoCompleter /*from ClassfileParser*/ | classfile.ClassfileParser#MemberCompleter | typer.Namer#Completer | LazyType#ProxyCompleter | ModuleCompleter], s"$this -- $myInfo -- ${myInfo.getClass}")
+      assert(!isCompleting || myInfo.isInstanceOf[unpickleScala2.Scala2Unpickler#LocalUnpickler | NoCompleter /*from ClassfileParser*/ | classfile.ClassfileParser#MemberCompleter | typer.Namer#Completer | LazyType#ProxyCompleter | ModuleCompleter | SymbolLoader], s"$this -- $myInfo -- ${myInfo.getClass}")
+      assert(isCompleted || myInfo.isInstanceOf[unpickleScala2.Scala2Unpickler#LocalUnpickler | NoCompleter /*from ClassfileParser*/ | classfile.ClassfileParser#MemberCompleter | typer.Namer#Completer | LazyType#ProxyCompleter | ModuleCompleter | SymbolLoader], s"$this -- $myInfo -- ${myInfo.getClass}")
       myPrivateWithin = sym
     }
 
@@ -542,8 +542,8 @@ object SymDenotations {
 
     /** Make denotation not exist */
     final def markAbsent(): Unit = {
-      assert(!isCompleting || myInfo.isInstanceOf[LazyType#ProxyCompleter | SymDenotations.ModuleCompleter | ClassfileLoader | typer.Namer#ClassCompleter | NoCompleter], s"$this -- $myInfo -- ${myInfo.getClass}")
-      assert(isCompleted || myInfo.isInstanceOf[LazyType#ProxyCompleter | SymDenotations.ModuleCompleter | ClassfileLoader | typer.Namer#ClassCompleter | NoCompleter], s"$this -- $myInfo -- ${myInfo.getClass}")
+      assert(!isCompleting || myInfo.isInstanceOf[LazyType#ProxyCompleter | SymDenotations.ModuleCompleter | ClassfileLoader | typer.Namer#ClassCompleter | SymbolLoader | NoCompleter], s"$this -- $myInfo -- ${myInfo.getClass}")
+      assert(isCompleted || myInfo.isInstanceOf[LazyType#ProxyCompleter | SymDenotations.ModuleCompleter | ClassfileLoader | typer.Namer#ClassCompleter | SymbolLoader | NoCompleter], s"$this -- $myInfo -- ${myInfo.getClass}")
       myInfo = NoType
     }
 
@@ -556,7 +556,7 @@ object SymDenotations {
     final def isAbsent(implicit ctx: Context): Boolean = {
       // ensureCompleted()
 
-      if (myInfo.isInstanceOf[LazyType#ProxyCompleter | SymDenotations.ModuleCompleter | ClassfileLoader/* | typer.Namer#ClassCompleter*/] ||
+      if (myInfo.isInstanceOf[LazyType#ProxyCompleter | SymDenotations.ModuleCompleter | SymbolLoader/* | typer.Namer#ClassCompleter*/] ||
         (myInfo.isInstanceOf[typer.Namer#Completer] && !myInfo.isInstanceOf[typer.Namer#ClassCompleter])) {
         // ensureCompleted()
         completeOnce()
