70200a518110553f8361f3ab8508644dcdec1eee
Guillaume Martres <smarter@ubuntu.com>
2020-04-24 19:01:52 +0200

    test complete

--- sbt-dotty/sbt-test/scala2-compat/types/scala2Lib/Api.scala
+++ sbt-dotty/sbt-test/scala2-compat/types/scala2Lib/Api.scala
@@ -1,4 +1,5 @@
-// package scala2Lib
+// Keep synchronized with ../dottyApp/Api.scala
+package scala2Lib
 
 class foo extends scala.annotation.StaticAnnotation
 
@@ -10,7 +11,14 @@ trait Cov[+T]
 
 class D
 
-class E {
+class VC(val self: A) extends AnyVal
+
+class Outer {
+  class E
+  trait F extends E
+}
+
+class Z {
   def a_01(a: A with B): Unit = {}
   def a_02(a: A with B with A): Unit = {}
   def a_03(a: A with (B with A)): Unit = {}
@@ -27,7 +35,7 @@ class E {
   type U <: T2 with S
   def b_08(b: U): Unit = {}
 
-  val sing: B = null
+  val sing: B = new B {}
   def a_09(a: A with sing.type): Unit = {}
   def b_10(b: sing.type with A): Unit = {}
 
@@ -46,7 +54,7 @@ class E {
 
   type DSub <: D
   def a_19(a: A with DSub): Unit = {}
-  def e_20(e: DSub with E): Unit = {}
+  def z_20(e: DSub with Z): Unit = {}
 
   type W1 <: A with Cov[Any]
   type X1 <: Cov[Int] with W1
@@ -56,15 +64,21 @@ class E {
   type X2 <: Cov[Int] with W2
   def a_22(a: X2): Unit = {}
 
-  def e_23(e: A with this.type): Unit = {}
-  def e_24(e: this.type with A): Unit = {}
+  def z_23(e: A with this.type): Unit = {}
+  def z_24(e: this.type with A): Unit = {}
 
-  // TODO: refinements, value classes, inner classes
-  // class Outer {
-  //   trait TraitX extends ClassY
-  //   class ClassY
-  // }
-  // object Api {
-  //   def foo(o1: Outer, o2: Outer): o1.ClassY with o2.TraitX = {}
-  // }
+  def b_25(b: A with (B { type T })): Unit = {}
+  def a_26(a: (A { type T }) with ((B with A) { type T })): Unit = {}
+
+  def a_27(a: VC with B): Unit = {}
+  def a_28(a: B with VC): Unit = {}
+
+  val o1: Outer = new Outer
+  val o2: Outer = new Outer
+  def f_29(a: o1.E with o1.F): Unit = {}
+  def f_30(a: o1.F with o1.E): Unit = {}
+  def f_31(a: o1.E with o2.F): Unit = {}
+  def f_32(a: o2.F with o1.E): Unit = {}
+  def f_33(a: Outer#E with Outer#F): Unit = {}
+  def f_34(a: Outer#F with Outer#E): Unit = {}
 }
