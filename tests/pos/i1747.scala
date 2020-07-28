43e49e3d014d5f781e61a57f086e5a1b72e41726
Guillaume Martres <smarter@ubuntu.com>
2020-07-28 20:11:03 +0200

    i1747 works more

--- tests/pos/i1747.scala
+++ tests/pos/i1747.scala
@@ -1,8 +1,13 @@
-abstract class Coll[E] extends java.util.Collection[E] {
+// TODO: check that all of those have same sig and work
 
-  // fails in elimevt check, can we get it to work? (why doesn't it fail earlier?)
-  // override def toArray[T](a: Array[T with Object]): Array[T with Object] = ???
-  override def toArray[T <: Object](a: Array[T]): Array[T] = ???
-  // override def toArray[T](a: Array[T]): Array[T] = ???
+abstract class Coll1[E] extends java.util.Collection[E] {
+  override def toArray[T](a: Array[T with Object]): Array[T with Object] = ???
 }
 
+abstract class Coll2[E] extends java.util.Collection[E] {
+  override def toArray[T <: Object](a: Array[T]): Array[T] = ???
+}
+
+abstract class Coll3[E] extends java.util.Collection[E] {
+  override def toArray[T](a: Array[T]): Array[T] = ???
+}
