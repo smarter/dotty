ee1e6475d2d64267e235e46d203c59335660139a
Guillaume Martres <smarter@ubuntu.com>
2020-12-22 17:42:38 +0100

    Remove widening that causes loop

--- compiler/src/dotty/tools/dotc/core/classfile/ClassfileParser.scala
+++ compiler/src/dotty/tools/dotc/core/classfile/ClassfileParser.scala
@@ -351,7 +351,7 @@ class ClassfileParser(
         case 'L' =>
           def processInner(tp: Type): Type = tp match {
             case tp: TypeRef if !tp.symbol.owner.is(Flags.ModuleClass) =>
-              TypeRef(processInner(tp.prefix.widen), tp.symbol.asType)
+              TypeRef(processInner(tp.prefix), tp.symbol.asType)
             case _ =>
               tp
           }
