c8fe6d59394e5e6b8ce4a5d10386a045e34c76a8
Guillaume Martres <smarter@ubuntu.com>
2021-01-08 17:21:59 +0100

    Use Denotation#matches instead of Type#matchesLoosely to decide if overriding
    Because this is what decides overrides in OverridingCursors

--- compiler/src/dotty/tools/dotc/typer/RefChecks.scala
+++ compiler/src/dotty/tools/dotc/typer/RefChecks.scala
@@ -523,11 +523,14 @@ object RefChecks {
         || mbr.is(JavaDefined) && hasJavaErasedOverriding(mbr)
 
       def isImplemented(mbr: Symbol) =
-        val mbrType = clazz.thisType.memberInfo(mbr)
+        // val mbrType = clazz.thisType.memberInfo(mbr)
         def isConcrete(sym: Symbol) = sym.exists && !sym.isOneOf(NotConcrete)
         clazz.nonPrivateMembersNamed(mbr.name)
           .filterWithPredicate(
-            impl => isConcrete(impl.symbol) && mbrType.matchesLoosely(impl.info))
+            impl => isConcrete(impl.symbol) && {
+              // mbrType.matchesLoosely(impl.info)
+              mbr.asSeenFrom(clazz.thisType).matches(impl)
+            })
           .exists
 
       /** The term symbols in this class and its baseclasses that are
