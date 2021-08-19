57b839f8cba029093bb119759f0a25faf7de03d2
Guillaume Martres <smarter@ubuntu.com>
2021-08-19 16:30:21 +0200

    fix i12284 but still todo
    [error] -- Error: /__w/dotty/dotty/community-build/community-projects/libretto/examples/src/main/scala/libretto/examples/PoolingMicroscopes.scala:98:13
    [error] 98 |      .>(pool)
    [error]    |             ^
    [error]    |ambiguous implicit arguments: both method signalMicroscopeReadiness in object Microscopes and method signalingJunctionPositiveVal in class StarterAppBase match type libretto.StarterKit.invertLib.coreLib.Signaling.Positive[A] of an implicit parameter of method pool in class StarterAppBase

--- tests/neg/i12284.scala
+++ tests/neg/i12284.scala
@@ -1,6 +1,8 @@
-trait I[F[_], A]  // error
+trait I[F[_], A]
 
-def magic[F[_], A](in: I[F, A]): F[A] =  // error // error
-  val deps: Vector[I[F, _]] = ??? // error
-  val xx: Vector[F[_]] = deps.map(i => magic(i)) // error // error // error
+def magic[F[_], A](in: I[F, A]): F[A] =
+  val deps: Vector[I[F, _]] = ???
+  // TODO: map gets inferred to ?B := [i.A] because we start with ?B >: F[?A] and only later get ?A := i.A
+  // see discussion in i8900a3.scala to deal with that.
+  val xx: Vector[F[_]] = deps.map(i => magic(i)) // error
   ???
