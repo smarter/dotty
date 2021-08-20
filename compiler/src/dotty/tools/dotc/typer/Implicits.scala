979a206af5e98bbfd40e942271ffd765a2e2dac2
Guillaume Martres <smarter@ubuntu.com>
2021-08-20 12:22:06 +0200

    wip

--- compiler/src/dotty/tools/dotc/typer/Implicits.scala
+++ compiler/src/dotty/tools/dotc/typer/Implicits.scala
@@ -591,8 +591,8 @@ trait ImplicitRunInfo:
               if ctx.typerState.constraint.contains(t) then provisional = true
             case t: TermParamRef =>
               traverse(t.underlying)
-            case t: FunProto =>
-              // skip
+            // case t: FunProto =>
+            //   // skip
             case t =>
               traverseChildren(t)
 
