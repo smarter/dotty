9ff40df926f37f359fcac17f8072890a6257aa89
Guillaume Martres <smarter@ubuntu.com>
2021-10-25 17:33:32 +0200

    remove unifying last effort because it breaks SnippetCompiler, instead avoid ranges with HK applications
        tests/neg/missing-implicit4.scala failed
        tests/neg/missing-implicit1.scala failed
        tests/neg-custom-args/isInstanceOf/i8932.scala failed
        tests/neg-custom-args/isInstanceOf/i11178.scala failed
        tests/neg-custom-args/isInstanceOf/i4297.scala failed
        tests/pos-special/isInstanceOf/gadt.scala failed

--- compiler/src/dotty/tools/dotc/core/ConstraintHandling.scala
+++ compiler/src/dotty/tools/dotc/core/ConstraintHandling.scala
@@ -98,8 +98,17 @@ trait ConstraintHandling {
       val dropWildcards = new AvoidWildcardsMap:
         @annotation.threadUnsafe lazy val localParamRefs = util.HashSet[Type]()
 
+        def desc = i"constraint $param ${if isUpper then "<:" else ":>"} $rawBound to\n$constraint"
+
+        override protected[core] def variance_=(x: Int): Unit =
+          // if variance != -1 && x == -1 then
+          //   println("@@: " + desc)
+          //   Thread.dumpStack
+          super.variance_=(x)
+
         if isUpper then variance = -1
         if flipVariance then variance = -variance
+
         // if isUpper then variance = -1
 
         // override def apply(t: Type): Type = t match
@@ -130,8 +139,6 @@ trait ConstraintHandling {
         def paramLevel = constraint.typeVarOfParam(param) match
           case tv: TypeVar => tv.nestingLevel
           case _ => Int.MaxValue
-
-        def desc = i"constraint $param ${if isUpper then "<:" else ":>"} $rawBound to\n$constraint"
 
         override def apply(tp: Type): Type = tp match
           // case tp: NamedType if (tp.symbol ne defn.TypeBox_CAP) && !tp.symbol.isStatic && tp.symbol.id > paramLevel =>
@@ -200,8 +207,36 @@ trait ConstraintHandling {
             localParamRefs ++= tl.paramRefs
             mapOver(tl)
 
+          case tp: AppliedType =>
+            val isHK = tp.tycon.isLambdaSub
+            derivedAppliedType(tp, this(tp.tycon), mapArgs(tp.args, tp.tyconTypeParams, isHK))
+
           case _ =>
+            // println(s"[$variance]map over: " + tp.show)
             super.apply(tp)
+
+        def mapArgs(args: List[Type], tparams: List[ParamInfo], isHK: Boolean): List[Type] = args match
+          case arg :: otherArgs if tparams.nonEmpty =>
+            val arg1 = arg match
+              case arg: TypeBounds => this(arg)
+              case arg => atVariance(variance * tparams.head.paramVarianceSign)(mapArg(arg, isHK))
+            val otherArgs1 = mapArgs(otherArgs, tparams.tail)
+            if ((arg1 eq arg) && (otherArgs1 eq otherArgs)) args
+            else arg1 :: otherArgs1
+          case nil =>
+            nil
+
+        def mapArg(tp: Type, isHKArg: Boolean): Type = tp match
+          case tp: TypeVar if isHKArg && variance == 0 && !tp.isInstantiated && tp.nestingLevel > paramLevel =>
+            // Don't use a range since hk applications can't be wildcards.
+            val bounds = if tp frozen_<:< defn.AnyType then TypeBounds.empty else TypeBounds.emptyPolyKind
+            val tvar = newTypeVar(bounds)
+            tvar.nestingLevel = paramLevel
+            assert(tp <:< tvar, i"$tp <:< $tvar -- $desc")
+            assert(tvar <:< tp, i"$tvar <:< $tp -- $desc")
+            tvar
+          case _ => apply(tp)
+
         override def mapWild(t: WildcardType) =
           if approximateWildcards then super.mapWild(t)
           else newTypeVar(apply(t.effectiveBounds).toBounds)
--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -5328,7 +5328,9 @@ object Types {
 
   /** Common base class of TypeMap and TypeAccumulator */
   abstract class VariantTraversal:
-    protected[core] var variance: Int = 1
+    private var myVariance: Int = 1
+    protected[core] def variance: Int = myVariance
+    protected[core] def variance_=(x: Int): Unit = myVariance = x
 
     inline protected def atVariance[T](v: Int)(op: => T): T = {
       val saved = variance
@@ -5436,6 +5438,7 @@ object Types {
             derivedSelect(tp, prefix1)
 
         case tp: AppliedType =>
+          // println("map over: " + tp)
           derivedAppliedType(tp, this(tp.tycon), mapArgs(tp.args, tp.tyconTypeParams))
 
         case tp: LambdaType =>
@@ -5757,7 +5760,10 @@ object Types {
               range(tp.derivedAppliedType(tycon, loBuf.toList),
                     tp.derivedAppliedType(tycon, hiBuf.toList))
             else
-              var saved = true
+              // Setting this to true breaks SnippetChecker, unless we disable flipVariance,
+              // but that breaks bootstrapping, etc.
+              var saved = false //true
+              // println(i"%[$variance] $tp -- $tycon -- $args")
               // TODO: just don't make Range arguments if application head is HK?
               val args2 = args.mapConserve {
                 case Range(a, b) =>
@@ -5865,8 +5871,10 @@ object Types {
     def apply(t: Type): Type = t match
       case t: WildcardType => mapWild(t)
       case _ =>
-        // println(s"mapping[$variance]: " + t.show)
-        mapOver(t)
+        // println(s"mapping[$variance]: " + t)
+        val z = mapOver(t)
+        // println(s"mapped[$variance]: " + t)
+        z
 
   // ----- TypeAccumulators ----------------------------------------------------
 
