28305a0cce274d3b90aea201f4400565464ba7a0
Guillaume Martres <smarter@ubuntu.com>
2021-11-05 18:12:19 +0100

    fix protoquill: quote type symbols behave like pattern-bound symbosl

--- compiler/src/dotty/tools/dotc/core/tasty/TreePickler.scala
+++ compiler/src/dotty/tools/dotc/core/tasty/TreePickler.scala
@@ -207,8 +207,8 @@ class TreePickler(pickler: TastyPickler) {
       }
       else if (tpe.prefix == NoPrefix) {
         writeByte(if (tpe.isType) TYPEREFdirect else TERMREFdirect)
-        if !symRefs.contains(sym) && !sym.isPatternBound then
-          report.error(i"pickling reference to as yet undefined $tpe with symbol ${sym}", sym.srcPos)
+        if !symRefs.contains(sym) && !sym.isPatternBound && !sym.hasAnnotation(defn.QuotedRuntimePatterns_patternTypeAnnot) then
+          report.warning(i"pickling reference to as yet undefined $tpe with symbol ${sym}", sym.srcPos)
         pickleSymRef(sym)
       }
       else tpe.designator match {
