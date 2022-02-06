3d57050be24103d2780ac947d43d163f8ac0c53b
Guillaume Martres <smarter@ubuntu.com>
2022-02-06 15:54:36 +0100

    wip

--- compiler/src/dotty/tools/dotc/core/classfile/ClassfileParser.scala
+++ compiler/src/dotty/tools/dotc/core/classfile/ClassfileParser.scala
@@ -121,9 +121,11 @@ class ClassfileParser(
   def classNameToSymbol(name: Name)(using Context): Symbol = innerClasses.get(name.toString) match {
     case Some(entry) =>
       val classSym = innerClasses.classSymbol(entry)
-      debugCD(name, (), printErr(s"classNameToSymbol (with inner) name=$name has symbol ${classSym} and loc=${classSym.showLocated}"))
+      // debugCD(name, (), printErr(s"classNameToSymbol (with inner) name=$name has symbol ${classSym} and loc=${classSym.showLocated}"))
       classSym
     case None =>
+      def lookupTopLevel(): Symbol = requiredClass(name)
+      
       // For inner classes we usually don't get to this branch: `innerClasses.classSymbol` already returns the symbol
       // of the inner class based on the InnerClass table. However, if the classfile is missing the
       // InnerClass entry for `name`, it might still be that there exists an inner symbol (because
@@ -131,36 +133,45 @@ class ClassfileParser(
       // return the actual inner symbol (C.D, with owner C), not the top-level symbol C$D. This is
       // what the logic below is for (see PR #5822 / scala/bug#9937) in Scala 2.
 
+      // println("#name: " + name)
       val nameAsString = name.toString
       val split = nameAsString.lastIndexOf('$')
-      val classSym =
-        if (split > 0 && split < nameAsString.length)
-          val outerNameStr = nameAsString.substring(0, split)
-          val innerNameStr = nameAsString.substring(split + 1, nameAsString.length)
-          val localizedName = s"$outerNameStr.$innerNameStr".toTypeName
-          val outerName = outerNameStr.toTypeName
-          val owner = requiredClass(outerName) // TODO: do we need something more general that requiredClass?
-          owner.denot.infoOrCompleter match
-            // If the outer class C cannot be found, look for a top-level class C$D
-            case _: StubInfo => requiredClass(name)
-            // We have a java-defined class name C$D and look for a member D of C. But we don't know if
-            // D is declared static or not, so we have to search both in class C and its companion.
-            case _ =>
-              // TODO: we need a non ClassSymbol "Symbol" of  owner, to test if it is a class symbol or not
-              // TODO: maybe a way to go from Name to Symbol?
-              // Note: use symbol.asClass when needed
-              val innerName = innerNameStr.toTypeName
-              staticRef(outerName).requiredSymbol("class", outerName)(_.isClass) match
-                case owner: ClassSymbol => staticScope.lookup(innerName) match
-                  case cs: ClassSymbol => cs
-                  case _ => requiredClass(localizedName).copy(owner = owner) // instanceScope.lookup(innerName)
-                case _ =>
-                   requiredClass(localizedName).copy(owner = owner) // TODO: not sure if this is what we want
-        else
-          requiredClass(name)
-
-      debugCD(name, (), printErr(s"classNameToSymbol (no inner) name=$name has symbol ${classSym} and loc=${classSym.showLocated}"))
-      classSym
+      if (split > 0 && split < nameAsString.length)
+        val outerNameStr = nameAsString.substring(0, split)
+        val innerNameStr = nameAsString.substring(split + 1, nameAsString.length)
+        val localizedName = s"$outerNameStr.$innerNameStr".toTypeName
+        val outerName = outerNameStr.toTypeName
+        // println("#name: " + name)
+        // println("#classRoot: " + classRoot)
+        // println("#outerName: " + outerName)
+        val outerSym = classNameToSymbol(outerName)
+        // println("#outerSym: " + outerSym.denot.infoOrCompleter)
+        outerSym match
+          // We have a java-defined class name C$D and look for a member D of C. But we don't know if
+          // D is declared static or not, so we have to search both in class C and its companion.
+          case outerSym: ClassSymbol =>
+            // TODO: we need a non ClassSymbol "Symbol" of  outerSym, to test if it is a class symbol or not
+            // TODO: maybe a way to go from Name to Symbol?
+            // Note: use symbol.asClass when needed
+            val innerName = innerNameStr.toTypeName
+            val r =
+              if outerSym eq classRoot.symbol then
+                instanceScope.lookup(innerName).orElse(staticScope.lookup(innerName))
+              else
+                outerSym.info.member(innerName).orElse(outerSym.companionModule.info.member(innerName)).symbol
+            r.orElse(lookupTopLevel())
+          //   staticRef(outerName).requiredSymbol("class", outerName)(_.isClass) match
+          //     case owner: ClassSymbol => staticScope.lookup(innerName) match
+          //       case cs: ClassSymbol => cs
+          //       case _ => requiredClass(localizedName).copy(owner = owner) // instanceScope.lookup(innerName)
+          //     case _ =>
+          //        requiredClass(localizedName).copy(owner = owner) // TODO: not sure if this is what we want
+          // // If the outer class C cannot be found, look for a top-level class C$D
+          case _ => lookupTopLevel()
+      else
+        lookupTopLevel()
+      // debugCD(name, (), printErr(s"classNameToSymbol (no inner) name=$name has symbol ${classSym} and loc=${classSym.showLocated}"))
+      // classSym
   }
 
   var sawPrivateConstructor: Boolean = false
