37f2b9e9199c8870ccc7dd637654459707a0a093
Guillaume Martres <smarter@ubuntu.com>
2022-10-05 11:18:49 +0200

    Java annotations do not extend scala.annotation.{ClassfileAnnotation,Annotation}

--- compiler/src/dotty/tools/dotc/core/Definitions.scala
+++ compiler/src/dotty/tools/dotc/core/Definitions.scala
@@ -961,8 +961,8 @@ class Definitions {
   def RuntimeTupleFunctionsModule(using Context): Symbol = requiredModule("scala.runtime.TupledFunctions")
 
   // Annotation base classes
+  @tu lazy val JavaAnnotationClass: ClassSymbol = requiredClass("java.lang.annotation.Annotation")
   @tu lazy val AnnotationClass: ClassSymbol = requiredClass("scala.annotation.Annotation")
-  @tu lazy val ClassfileAnnotationClass: ClassSymbol = requiredClass("scala.annotation.ClassfileAnnotation")
   @tu lazy val StaticAnnotationClass: ClassSymbol = requiredClass("scala.annotation.StaticAnnotation")
   @tu lazy val RefiningAnnotationClass: ClassSymbol = requiredClass("scala.annotation.RefiningAnnotation")
 
