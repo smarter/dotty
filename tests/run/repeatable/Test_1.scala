1f6fecde3b1585da76461738c8662402d517d43a
Guillaume Martres <smarter@ubuntu.com>
2022-10-07 20:44:21 +0200

    TODO
        tests/neg/repeatable failed
        tests/run/repeatable failed
    
    @Ann(Array(Ann1()))
    @Ann(Ann2())) // new Ann2

--- tests/run/repeatable/Test_1.scala
+++ tests/run/repeatable/Test_1.scala
@@ -7,16 +7,19 @@ import repeatable._
 @Plain_0(3)
 trait U
 
-@FirstLevel_0(Array(Plain_0(4), Plain_0(5)))
-@FirstLevel_0(Array(Plain_0(6), Plain_0(7)))
+@FirstLevel_0(Array(new Plain_0(4), new Plain_0(5)))
+@FirstLevel_0(Array(new Plain_0(6), new Plain_0(7)))
 trait T
 
-object Test:
-  def main(args: Array[String]) =
+object Test {
+  def main(args: Array[String]) = {
+    val x = new Plain_0(4)
     val annValuesU = classOf[U].getAnnotation(classOf[FirstLevel_0]).value.toList.map(_.value).sorted
     annValuesU.foreach(println)
 
     println()
 
-    val annValuesT = classOf[T].getAnnotation(classOf[SecondLevel_0]).value.toList.map(_.value.toList.map(_.value).sorted).sorted
+    val annValuesT = classOf[T].getAnnotation(classOf[SecondLevel_0]).value.toList.map(_.value.toList.map(_.value).sorted)//.sorted
     annValuesT.foreach(println)
+  }
+}
