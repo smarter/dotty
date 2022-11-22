c39c56e88dd57a799d3d8da6bc98f85c4f323c80
Guillaume Martres <smarter@ubuntu.com>
2022-11-22 19:03:01 +0100

    fix instDirection to be coherent with non-param instantiation

--- compiler/src/dotty/tools/dotc/core/TypeComparer.scala
+++ compiler/src/dotty/tools/dotc/core/TypeComparer.scala
@@ -3002,6 +3002,9 @@ object TypeComparer {
   def fullBounds(param: TypeParamRef)(using Context): TypeBounds =
     comparing(_.fullBounds(param))
 
+  def nonParamBounds(param: TypeParamRef)(using Context): TypeBounds =
+    comparing(_.nonParamBounds(param))
+
   def fixLevels(tp: Type, fromBelow: Boolean, maxLevel: Int, param: TypeParamRef)(using Context) =
     comparing(_.fixLevels(tp, fromBelow, maxLevel, param))
 
