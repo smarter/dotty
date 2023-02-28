f70074cc410a1b7b6398e329463e9b0fd2b00e44
Guillaume Martres <smarter@ubuntu.com>
2023-02-28 16:01:33 +0100

    Revert "optimized"
    This reverts commit 0fcbbdcb686c58f4058c65361064ce2f67bfe4f2.

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -1574,6 +1574,8 @@ object Types {
           else NoType
         case SkolemType(tp) =>
           loop(tp)
+        case pre: WildcardType =>
+          WildcardType.sameKindAs(pre.effectiveBounds.hi.select(name))
         case pre: TypeRef =>
           pre.info match {
             case TypeAlias(alias) => loop(alias)
@@ -2651,7 +2653,6 @@ object Types {
     def derivedSelect(prefix: Type)(using Context): Type =
       if (prefix eq this.prefix) this
       else if (prefix.isExactlyNothing) prefix
-      else if (prefix.isInstanceOf[WildcardType]) WildcardType.sameKindAs(this)
       else {
         if (isType) {
           val res =
@@ -2678,7 +2679,11 @@ object Types {
               case _ =>
             }
         }
-        withPrefix(prefix)
+        prefix match
+          case _: WildcardType =>
+            WildcardType.sameKindAs(this)
+          case _ =>
+            withPrefix(prefix)
       }
 
     /** A reference like this one, but with the given symbol, if it exists */
