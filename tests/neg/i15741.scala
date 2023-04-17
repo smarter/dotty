057636de7937f351957a973e5f20c934aa137944
Guillaume Martres <smarter@ubuntu.com>
2023-04-17 18:30:51 +0200

    Drop specific check for context function in result of PF
    No longer needed after refactoring SAMType handling of PF since for regular
    SAMs we were already checking this.
    
    Two of the extra errors are a bit confusing though.

--- tests/neg/i15741.scala
+++ tests/neg/i15741.scala
@@ -1,15 +1,15 @@
   def get(using Int): String = summon[Int].toString
 
   def pf2: PartialFunction[String, Int ?=> String] = {
-    case "hoge" => get // error
-    case "huga" => get
+    case "hoge" => get // error // error
+    case "huga" => get // error
   }
 
   type IS = Int ?=> String
 
   def pf3: PartialFunction[String, IS] = {
-    case "hoge" => get  // error
-    case "huga" => get
+    case "hoge" => get  // error // error
+    case "huga" => get  // error
   }
 
 
