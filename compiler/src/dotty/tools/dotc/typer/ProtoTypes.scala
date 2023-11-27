ecd9a61815285ab631a511bc25a2bf04a61f9cd7
Guillaume Martres <smarter@ubuntu.com>
2023-11-28 00:23:14 +0100

    rec bounds to handle tests/run/i10930.scala

--- compiler/src/dotty/tools/dotc/typer/ProtoTypes.scala
+++ compiler/src/dotty/tools/dotc/typer/ProtoTypes.scala
@@ -819,12 +819,14 @@ object ProtoTypes {
         override def apply(tp: Type): Type = tp match
           case tp @ TypeRef(prefix: TermParamRef, _)
           if !tp.symbol.isClass && prefix.binder == mt && replacements.contains(prefix) =>
+            val origBounds = tp.info.bounds
             val memberVar = memberVars.getOrElseUpdate(tp.symbol, {
               // TODO: tp.info shoudl be traversed regularly since we might have type Elem <: this.Bla
-              val newVar = newTypeVar(tp.info.bounds)
+              val newVar = newTypeVar(TypeBounds.emptySameKindAs(origBounds))
               replacements(prefix) = RefinedType(replacements(prefix), tp.name, TypeAlias(newVar))
               newVar
             })
+            apply(origBounds).asInstanceOf[TypeBounds].contains(memberVar)
             memberVar
           case _ => mapOver(tp)
       val z = approx(mt.resultType).substParams(mt, //replacements.values.toList)
